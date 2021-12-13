c9019f9eb198ef39e4d68b629960c0d697fc246b
Vitalii Pikozh <wisart@gmail.com>
2021-12-13 22:37:19 +0200

    add README.md

---
+++ README.md
@@ -0,0 +1,25 @@
+Скриншоты результата выполнения каждой команды:
+
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+node index.js --action list
+
+https://ibb.co/6bs3JwP
+
+# Получаем контакт по id
+
+node index.js --action get --id 5
+
+https://ibb.co/1mhBg12
+
+# Добавялем контакт
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+https://ibb.co/nbGd0St
+
+# Удаляем контакт
+
+node index.js --action remove --id=3
+
+https://ibb.co/K6y15xZ
---
+++ contacts.js
@@ -0,0 +1,38 @@
+const fs = require("fs/promises");
+const path = require("path");
+const crypto = require("crypto");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+const readContent = async () => {
+  const content = await fs.readFile(contactsPath, "utf8");
+  const result = JSON.parse(content);
+  return result;
+};
+
+const listContacts = async () => {
+  return await readContent();
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await readContent();
+  const result = contacts.filter((contact) => contact.id === contactId);
+  return result;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await readContent();
+  const result = contacts.filter((contact) => contact.id !== contactId);
+  await fs.writeFile(contactsPath, JSON.stringify(result, null, 2));
+  return result;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await readContent();
+  const newContact = { name, email, phone, id: crypto.randomUUID() };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
